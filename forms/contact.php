a5a8f7ea60b141bfeb6264bb3f6516469f162898
parinithgowda98 <118339150+parinithgowda98@users.noreply.github.com>
2023-02-03 13:29:48 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -19,9 +19,9 @@
   $contact->ajax = true;
 
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->from_name = $_GET['name'];
+  $contact->from_email = $_GET['email'];
+  $contact->subject = $_GET['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +33,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_GET['name'], 'From');
+  $contact->add_message( $_GET['email'], 'Email');
+  $contact->add_message( $_GET['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
